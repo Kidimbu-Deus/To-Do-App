071b8db35661a54bb6f797964b654042816797b1
Kidimbu <kidimbujunior2@gmail.com>
2022-06-24 23:53:58 +0200

    create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let submitBtn = document.getElementById("new-task-submit")
+
+    submitBtn.onclick = function (e) {
+
+    e.preventDefault()
+
+    let validated = true;
+
+    let newTaskInput = document.getElementById("new-task-input")
+
+    
+    
+}
