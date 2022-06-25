1cb899d3c3b2409a4b3235a36fdf360fddbed3eb
Kidimbu <kidimbujunior2@gmail.com>
2022-06-26 00:27:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 window.addEventListener('load', () =>{
     taskList = JSON.parse(this.localStorage.getItem('todos')) || [];
-const nameInput = document.querySelector('#name')
-const newTaskForm = document.querySelector('#new-task')
+const nameInput = document.querySelector('#name');
+const newTaskForm = document.querySelector('#new-task');
+
+const username = localStorage.getItem('username') || '';
+
+nameInput.value = username;
+
+nameInput.addEventListener('change', e => {
+    localStorage.setItem('username', e.target.value);
+    })
 })
