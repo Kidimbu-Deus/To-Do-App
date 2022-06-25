94c16d1f54288812ef35602cd92c7073771cfbaa
Kidimbu <kidimbujunior2@gmail.com>
2022-06-25 03:17:15 +0200

    newJavascript
    Setting up javascript. First Added a localStorae

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-window.addEventListener 
-    
-
+window.addEventListener('load', () =>{
+    taskList = JSON.parse(this.localStorage.getItem('todos')) || [];
+const nameInput = document.querySelector('#name')
+const newTaskForm = document.querySelector('#new-task')
+})
