6b8188910d5bd00ef73b0337aedf3df46c0e2340
Kidimbu <kidimbujunior2@gmail.com>
2022-06-26 01:58:49 +0200

    taskInput
    Added 'submit' eventListener
    Added callback function to prevent default upon submission
    Created localStorage for taskInput's

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', () =>{
-    taskList = JSON.parse(this.localStorage.getItem('todos')) || [];
+window.addEventListener("load", () => {
+    taskList = JSON.parse(this.localStorage.getItem('taskList')) || [];
 const nameInput = document.querySelector('#name');
 const newTaskForm = document.querySelector('#new-task');
 
@@ -7,7 +7,27 @@ const username = localStorage.getItem('username') || '';
 
 nameInput.value = username;
 
-nameInput.addEventListener('change', e => {
+nameInput.addEventListener("change", e => {
     localStorage.setItem('username', e.target.value);
+})
+
+    newTaskForm.addEventListener("submit", e => {
+        e.preventDefault();
+    
+
+    const tasks = {
+        content: e.target.elements.task-content.value,
+        category: e.target.elements.category.value,
+        done: false,
+        createdAt: new Date().getTime
+    }
+
+    taskList.push(tasks);
+
+    localStorage.setItem('taskList', JSON,stringify(taskList))
+
+    e.target.reset()
+
     })
+
 })
