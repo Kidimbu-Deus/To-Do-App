fc9a976a765717cb4085c942c20c2b37a33d39f0
Kidimbu <kidimbujunior2@gmail.com>
2022-06-25 02:13:19 +0200

    Update main.js
    Restarting javascript

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-let submitBtn = document.getElementById("new-task-submit")
 
-    submitBtn.onclick = function (e) {
-
-    e.preventDefault()
-
-    let validated = true;
-
-    let newTaskInput = document.getElementById("new-task-input");
-
-    let list = document.createElement("li");
-    let textNode = document.createTextNode(document.getElementById("new-task-input").value);
-    list.appendChild(textNode);
-    document.getElementById("task-ist").appendChild(list);
     
-    
-}
+
