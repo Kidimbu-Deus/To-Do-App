04b8e2312bacda7dcafecea1f0276cd24044a356
Kidimbu <kidimbujunior2@gmail.com>
2022-06-26 02:53:18 +0200

    structureUpdate
    "task" class name change
    Display of task list and type of task

--- main.js
+++ main.js
@@ -15,19 +15,53 @@ nameInput.addEventListener("change", e => {
         e.preventDefault();
     
 
-    const tasks = {
+    const task = {
         content: e.target.elements.task-content.value,
         category: e.target.elements.category.value,
         done: false,
         createdAt: new Date().getTime
     }
 
-    taskList.push(tasks);
+    taskList.push(task);
 
     localStorage.setItem('taskList', JSON,stringify(taskList))
 
     e.target.reset()
 
+    DisplayTaskLists()
+
     })
 
+    DisplayTaskLists()
+
 })
+
+function DisplayTaskLists() {
+    const taskItems = documnet.getElementById('task-list')
+
+    taskItems.innerHTML = ''
+
+    taskList.forEach(task => {
+        const taskItem = document.createElement('div')
+        taskItem.classList.add('task-item')
+
+        const label = document.createElement('label')
+        const input = document.createElement('input')
+        const span = document.createElement('span')
+        const content = document.createElement('div')
+        const actions = document.createElement('div')
+        const edit = document.createElement('button')
+        const deleteButton = document.createElement('button')
+
+        input.type = 'checkbox'
+        input.checked = task.done
+        span.classList.add('bubble')
+
+        if (todo.category == 'personal'){
+            span.classList.add('personal')
+        } else {
+            span.classList.add('work')
+        }
+    })
+
+}
