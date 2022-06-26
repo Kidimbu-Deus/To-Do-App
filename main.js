3e3a1e594299bba2014a5835ccb3e0b0b6e3b597
Kidimbu <kidimbujunior2@gmail.com>
2022-06-26 08:55:03 +0200

    updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", () => {
-    taskList = JSON.parse(this.localStorage.getItem('taskList')) || [];
+    window.addEventListener("load", () => {
+taskList = JSON.parse(this.localStorage.getItem('taskList')) || [];
 const nameInput = document.querySelector('#name');
 const newTaskForm = document.querySelector('#new-task');
 
@@ -7,11 +7,11 @@ const username = localStorage.getItem('username') || '';
 
 nameInput.value = username;
 
-nameInput.addEventListener("change", e => {
+nameInput.addEventListener('change', e => {
     localStorage.setItem('username', e.target.value);
 })
 
-    newTaskForm.addEventListener("submit", e => {
+    newTaskForm.addEventListener('submit', e => {
         e.preventDefault();
     
 
@@ -19,49 +19,103 @@ nameInput.addEventListener("change", e => {
         content: e.target.elements.task-content.value,
         category: e.target.elements.category.value,
         done: false,
-        createdAt: new Date().getTime
+        createdAt: new Date().getTime(),
     }
 
     taskList.push(task);
 
-    localStorage.setItem('taskList', JSON,stringify(taskList))
+    localStorage.setItem('taskList', JSON.stringify(taskList));
 
-    e.target.reset()
+    e.target.reset();
 
-    DisplayTaskLists()
+    DisplayTaskList();
 
     })
 
-    DisplayTaskLists()
+    DisplayTaskList();
 
 })
 
-function DisplayTaskLists() {
-    const taskItems = documnet.getElementById('task-list')
-
-    taskItems.innerHTML = ''
+function DisplayTaskList() {
+    const taskItems = document.querySelector('#task-list');
 
     taskList.forEach(task => {
-        const taskItem = document.createElement('div')
-        taskItem.classList.add('task-item')
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task-item');
 
-        const label = document.createElement('label')
-        const input = document.createElement('input')
-        const span = document.createElement('span')
-        const content = document.createElement('div')
-        const actions = document.createElement('div')
-        const edit = document.createElement('button')
-        const deleteButton = document.createElement('button')
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
 
-        input.type = 'checkbox'
-        input.checked = task.done
-        span.classList.add('bubble')
+        input.type = 'checkbox';
+        input.checked = task.done;
+        span.classList.add('bubble');
 
-        if (todo.category == 'personal'){
-            span.classList.add('personal')
+        if (task.category == 'personal'){
+            span.classList.add('personal');
         } else {
-            span.classList.add('work')
+            span.classList.add('work');
         }
+
+        content.classList.add('content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${task.content}" readonly>`;
+        edit.innerHTML = 'edit';
+        deleteButton.innerHTML = 'delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        taskItem.appendChild(label);
+        taskItem.appendChild(content);
+        taskItem.appendChild(actions);
+
+        taskItems.appendChild(taskItem);
+
+        if (task.done) {
+            taskItem.classList.add('done');
+        }
+
+        input.addEventListener('click', e => {
+            task.done = e.target.checked;
+            localStorage.setItem('taskList', JSON.stringify(taskList))
+
+            if (task.done){
+                taskItem.classList.add('done');
+            } else {
+                taskItem.classList.remove('done');
+            }
+
+            DisplayTaskList();
+        })
+
+        edit.addEventListener('click', e => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e =>{
+                input.setAttribute('readonly', true);
+                task.content = e.target.value;
+                localStorage.setItem('taskList', JSON.stringify(taskList))
+                DisplayTaskList()
+
+            })
+
+        })
+
+        deleteButton.addEventListener('click', e =>{
+            taskList = taskList.filter(t => t != task);
+            localStorage.setItem('taskList', JSON.stringify(taskList))
+            DisplayTaskList();
+        })
     })
 
 }
